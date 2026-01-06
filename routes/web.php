db0ecfc892e4917ff6d8813734ba78d98ac1fbb0
Thierry Santos <thierrysantosd0@gmail.com>
2026-01-06 00:49:25 -0300

    Herança da classe Controller do Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\admin\AdminController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+
+Route::get('/teste/{value}', [MainController::class, 'teste']);
