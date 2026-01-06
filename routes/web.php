17f9bd1b2ba0c4a1174a7728249d2dcf41af8c37
Thierry Santos <thierrysantosd0@gmail.com>
2026-01-06 00:20:38 -0300

    como organizar as pastas do controller

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\SingleActionController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/init', [MainController::class, 'initMethod'])->name('init');
-Route::get('view', [MainController::class, 'viewPage'])->name('view');
-
-// Route para controller single action
-Route::get('/single', SingleActionController::class)->name('single');
-
-// Route para controller do tipo resource
-Route::resource('users', UserController::class);
-
-Route::resources([
-  'clients' => ClientsController::class,
-  'products' => ProductsController::class,
-  'users' => UserController::class
-]);
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
